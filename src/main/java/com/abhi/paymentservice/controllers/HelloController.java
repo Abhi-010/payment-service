fa41129e68a3eb072164cc2fc537ed9e8251be0f
Abhishek <abhishek.km010@gmail.com>
2025-08-11 12:54:09 +0530

    hello controller

---
+++ src/main/java/com/abhi/paymentservice/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.abhi.paymentservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    @GetMapping("/{name}")
+    public String sayHello(@PathVariable  String name){
+        return "hello " + name;
+    }
+}
